915d4cf3db2c87beb35daadecf285a50be4cd15d
Jaden Padua <paduajaden@gmail.com>
2021-01-26 12:29:16 -0800

    Thank you text on form submit

--- frontend/src/components/SplashContent/SplashContent.tsx
+++ frontend/src/components/SplashContent/SplashContent.tsx
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
 import cat3 from "../../assets/cat_3.png";
 import "./styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCat } from "@fortawesome/free-solid-svg-icons";
 
 const SplashContent: React.FC = () => {
   const [email, setEmail] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+  const onChange = (e: React.FormEvent<HTMLInputElement>): void => {
     setEmail(e.currentTarget.value);
   };
 
-  const onSubmit = (e: React.SyntheticEvent) => {
+  const validate = (email: string): boolean => {
+    // TODO: Email validation
+    return true;
+  };
+
+  const onSubmit = (e: React.SyntheticEvent): void => {
     e.preventDefault();
-    console.log(email);
+    if (validate(email)) {
+      console.log(email);
+      setEmail("");
+      setIsSubmitted(true);
+    } else {
+      console.log("Validation is false, send alert prompt...");
+    }
   };
 
   return (
@@ -26,25 +40,33 @@ const SplashContent: React.FC = () => {
         <h1>COMING SOON</h1>
         <h3>ARE YOU READY?</h3>
       </div>
-      <div className="container2">
-        <form className="input-form">
-          <label>
+
+      {isSubmitted ? (
+        <div className="thank-you">
+          Success! thank you for believing in Litter{" "}
+          <FontAwesomeIcon icon={faCat} className="icon" />
+        </div>
+      ) : (
+        <div className="container2">
+          <form className="input-form">
+            <label>
+              <input
+                onChange={onChange}
+                className="input-text"
+                type="text"
+                name="email"
+                placeholder="YOUR EMAIL"
+              />
+            </label>
             <input
-              onChange={onChange}
-              className="input-text"
-              type="text"
-              name="email"
-              placeholder="YOUR EMAIL"
+              onClick={onSubmit}
+              className="input-button"
+              type="submit"
+              value="NOTIFY ME"
             />
-          </label>
-          <input
-            onClick={onSubmit}
-            className="input-button"
-            type="submit"
-            value="NOTIFY ME"
-          />
-        </form>
-      </div>
+          </form>
+        </div>
+      )}
     </>
   );
 };
